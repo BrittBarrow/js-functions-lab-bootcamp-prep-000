e468b86ff1baf453990e6d2aa05d78c1c734123b
Brittany Barrow <bangelabarrow@gmail.com>
2018-03-02 00:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  returns `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
